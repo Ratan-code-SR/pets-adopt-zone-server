55bcd60bc878f8fb9a1f8edaa113fe221e1bf028
ratan <ratanchakma68@gmail.com>
2024-06-04 10:32:26 +0600

    users api added

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
-const corsOptions = {
-    origin: [
-        `${process.env.HTTP_URL}`,
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:5173/',
+//     'http://localhost:5174/',
 
-    ],
-    credentials: true,
-    optionSuccessStatus: 200,
-}
 
-app.use(cors(corsOptions))
+//   ],
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// }
+
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
@@ -33,8 +35,41 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-  
+    const usersCollection = client.db("petZoneDB").collection("users");
 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+
+    })
+    app.patch("/users/admin/:id",  async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+
+    })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "user already exist", insertedId: null })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -43,11 +78,11 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send('server is running')
+  res.send('server is running')
 })
 
 app.listen(port, (req, res) => {
-    console.log(`server is running at: http://localhost:${port}`);
+  console.log(`server is running at: http://localhost:${port}`);
 })
 
 
